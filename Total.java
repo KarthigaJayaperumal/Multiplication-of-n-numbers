3ffa6037e6892d532014aa62f6035b4da542f076
Karthiga Jayaperumal <99970828+KarthigaJayaperumal@users.noreply.github.com>
2022-04-10 18:17:29 +0530

    Multiplication Of n Numbers

---
+++ Total.java
@@ -0,0 +1,18 @@
+
+
+public class Total {
+
+	public static void main(String[] args) {
+		
+int no1=1;
+int total1 =1;
+
+while(no1<=5) {
+
+total1=total1*no1;
+no1++;
+}
+System.out.println("Muliplication of n numbers is "+total1);
+}
+
+}
